80a8e2571d6d5fc8e5510c6ec3af4303325d789e
ohganed <64299676+ohganed@users.noreply.github.com>
2026-08-28 10:40:28 +0900

    Cache Learning OS v8 control architecture

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='learning-os-v7';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./style-v3.css','./db-v3.js','./app-v3.js','./cognitive-v4.js','./cognitive-v5.js','./visual-v6.js','./visual-v7.js'];
+const CACHE='learning-os-v8';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./style-v3.css','./db-v3.js','./app-v3.js','./cognitive-v4.js','./cognitive-v5.js','./visual-v6.js','./visual-v7.js','./control-v8.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
